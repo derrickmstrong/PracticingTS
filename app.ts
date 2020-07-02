2d19b9d0fdb31140b5ddfef902155477582e6ed5
Derrick Strong <derrickmstrong@gmail.com>
2020-07-02 12:49:19 -0500

    Moved JS_Kitchen to root

---
+++ app.ts
@@ -0,0 +1,73 @@
+// Incomplete conversion (.js to .ts)
+// Variable "name" is assigned the value of "Derrick"
+let name: string = 'Derrick M';
+
+const numStates = 50;
+let sum = 5 + 4;
+
+sayHello();
+
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
+
+let favVeggies = ['carrots', 'cabbage', 'spinach', 'green peppers'];
+
+for (const veggies of favVeggies) {
+  console.log(veggies);
+}
+
+const pet = {
+  petname: 'GigaBite',
+  breed: 'Chia',
+};
+console.log(pet);
+
+const visitors = [
+  {
+    firstname: 'Luke',
+    age: 21,
+  },
+  {
+    firstname: 'Paul',
+    age: 39,
+  },
+  {
+    firstname: 'Kim',
+    age: 18,
+  },
+  {
+    firstname: 'Sally',
+    age: 14,
+  },
+  {
+    firstname: 'Sam',
+    age: 28,
+  },
+];
+
+for (const visitor of visitors) {
+  checkAge(visitor.firstname, visitor.age);
+}
+
+const length: number = getLength('Hello World');
+
+length % 2 === 0
+  ? console.log('The world is nice and even!')
+  : console.log('The world is an odd place!');
+
+// Function Declarations
+function sayHello() {
+  alert('Hello World!');
+}
+
+function checkAge(familyName, age) {
+  if (age < 21) {
+    alert('Sorry ' + familyName + ", you aren't old enough to view this page!");
+  }
+}
+
+function getLength(str) {
+  return str.length;
+}
